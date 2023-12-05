e5a9546dd47f9f7420b04697b910e8f68234bdb2
Red-Rim <salmaouakhir20@gmail.com>
2023-12-05 01:33:15 +0100

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,8 @@
-#add ......
-exec { 'configure-nginx':
-  command     => 'apt-get update && apt-get install -y nginx',
-  path        => '/usr/bin',
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/sites-available/default'],
+#add 
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => template('path/to/nginx_config_template.erb'),
-  notify  => Exec['restart-nginx'],
-}
-
-exec { 'restart-nginx':
-  command     => 'service nginx restart',
-  path        => '/usr/bin',
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/sites-available/default'],
-}
-
