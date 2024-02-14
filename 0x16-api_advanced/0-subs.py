2fa6638ceaa1c4e12dbbe072559e6eb0b5824298
Red-Rim <salmaouakhir20@gmail.com>
2024-02-14 22:06:19 +0100

    how many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""How many subs"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers"""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Chrome'}
-
-    try:
-        response = requests.get(url, headers=headers)
-
-        data = response.json()
-        find_subscribers = data.get("data", {}).get("subscribers", 0)
-        return find_subscribers
-    except requests.HTTPError as e:
-        if e.response.status_code == 404:
-            return 0
-        else:
-            return 0
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "underscoDe@alx-holbertonschool"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
