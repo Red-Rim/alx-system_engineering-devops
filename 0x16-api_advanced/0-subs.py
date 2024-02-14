dae4eb5cf547f4e83bfe351128c17dcb4d7c871a
Red-Rim <salmaouakhir20@gmail.com>
2024-02-14 22:03:25 +0100

    how many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,21 @@
+#!/usr/bin/python3
+"""How many subs"""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
+    """Returns the number of subscribers"""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Chrome'}
+
+    try:
+        response = requests.get(url, headers=headers)
+
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
-
-# Test the function
-if __name__ == "__main__":
-    subreddit = input("Enter the subreddit name: ")
-    print(number_of_subscribers(subreddit))
-
+        find_subscribers = data.get("data", {}).get("subscribers", 0)
+        return find_subscribers
+    except requests.HTTPError as e:
+        if e.response.status_code == 404:
+            return 0
+        else:
+            return 0
