99532f5851a9cb72e5b2b46e485e95466c8b95ea
Red-Rim <salmaouakhir20@gmail.com>
2024-02-14 21:57:15 +0100

    red-me

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
+
+# Test the function
+if __name__ == "__main__":
+    subreddit = input("Enter the subreddit name: ")
+    print(number_of_subscribers(subreddit))
+
