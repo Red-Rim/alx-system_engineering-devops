976ee0ac296056111bf73b0956c925a105c5d909
Red-Rim <salmaouakhir20@gmail.com>
2024-03-22 22:43:20 +0000

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
 """
-Module-level documentation.
+this doc for module
 """
-
 import requests
 
 headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
 def number_of_subscribers(subreddit):
-    """
-    Return the total number of subscribers on a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The total number of subscribers. Returns 0 if the subreddit is invalid or not found.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
         data = response.json()
         return data["data"]["subscribers"]
-    elif response.status_code == 404:
-        return 0
     else:
-        # Handle other potential error cases gracefully
         return 0
