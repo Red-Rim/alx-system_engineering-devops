dc07eea4ea78357a97396659944d51b5e6890914
Red-Rim <salmaouakhir20@gmail.com>
2024-03-23 00:52:12 +0000

    min op

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,14 +19,25 @@ def number_of_subscribers(subreddit):
         int: The number of subscribers for the subreddit.
         If the subreddit is not valid, returns 0.
     """
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Custom"}
 
     try:
+
         response = requests.get(url, headers=headers, allow_redirects=False)
+
+        print("API Request URL:", url)
+        print("API Response Content:", response.content)
+
         if response.status_code == 200:
             return response.json()["data"]["subscribers"]
         else:
             return 0
     except requests.RequestException:
+
         return 0
+
+
+subreddit = "python"
+print("Number of subscribers for r/python:", number_of_subscribers(subreddit))
